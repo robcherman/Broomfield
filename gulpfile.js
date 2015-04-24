d8f239ba517a5d11ba42ce10fba673f37c338310
chrisdatlas <christopherd@atlas-advertising.com>
2015-04-24 09:32:24 -0600

    Various tweaks to home page

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,76 @@
-var gulp = require('gulp'),
-	sass = require('gulp-ruby-sass'),
-	prefix = require('gulp-autoprefixer'),
-	ftp = require('gulp-ftp');
+// ============ broomfield ==============
 
-// defines path to sass
-var sassRoot = 'build/sass/';
+// Include gulp
+var gulp = require('gulp');
 
-// gulp task
-gulp.task('sass-to-css', function() {
-	return gulp.src(sassRoot + 'main.scss')
-		.pipe(sass({sourcemap: true}))
-		.pipe(prefix("last 3 versions"))
-		.pipe(gulp.dest('public/css'));
+// Include Plugins
+var ftp = require('gulp-ftp');
+var sass = require('gulp-ruby-sass');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+
+// Compile Sass
+gulp.task('sass', function() {
+  return gulp.src('build/sass/main.scss')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(sass({style: 'compressed'}))
+    .pipe(gulp.dest('css'));
 });
 
-// CSS ftp task
-gulp.task('ftp-css', ['sass-to-css'], function() {
-	return gulp.src('public/css/*.css')
-		.pipe(ftp({
-			host: '66.241.194.6',
-			user: 'zackp',
-			pass: 'aazackp',
-			remotePath: 'Broomfield/css'
-		}));
+// Dev FTP: CSS
+gulp.task('ftp-dev-css', function() {
+  return gulp.src('css/*')
+    .pipe(ftp({
+      host: '66.241.194.6',
+      user: 'chrisd',
+      pass: 'aachrisd',
+      remotePath: 'broomfield/css'
+    }));
 });
 
-// CSS ftp task
-gulp.task('ftp-js', function() {
-	return gulp.src('public/js/*.js')
-		.pipe(ftp({
-			host: '66.241.194.6',
-			user: 'zackp',
-			pass: 'aazackp',
-			remotePath: 'Broomfield/js'
-		}));
+// Dev FTP: Sass
+gulp.task('ftp-dev-scss', function() {
+  return gulp.src('scss/*')
+    .pipe(ftp({
+      host: '66.241.194.6',
+      user: 'chrisd',
+      pass: 'aachrisd',
+      remotePath: 'broomfield/scss'
+    }));
 });
 
-// watch
+// Prod FTP: CSS
+gulp.task('ftp-css', function() {
+  return gulp.src('css/*')
+    .pipe(ftp({
+      host: '66.241.194.5',
+      user: 'chrisd',
+      pass: 'G00berz',
+      remotePath: '/Atlas/Sites/KenticoCMS 7.0.103/broomfield/css'
+    }));
+});
+
+// Prod FTP: Sass
+gulp.task('ftp-scss', function() {
+  return gulp.src('scss/*')
+    .pipe(ftp({
+      host: '66.241.194.5',
+      user: 'chrisd',
+      pass: 'G00berz',
+      remotePath: '/Atlas/Sites/KenticoCMS 7.0.103/broomfield/scss'
+    }));
+});
+
+// FTP Everything to Dev and Prod
+gulp.task('ftp-all', ['ftp-dev-css', 'ftp-dev-scss', 'ftp-dev-scripts', 'ftp-css', 'ftp-scss', 'ftp-scripts']);
+
+// Watch Files For Changes
 gulp.task('watch', function() {
-	// what to watch
-	gulp.watch('build/sass/*.scss', function() {
-		// what to run
-		gulp.run('ftp-css');
-	});
-	gulp.watch('public/js/*.js', function() {
-		gulp.run('ftp-js');
-	})
+  gulp.watch('build/sass/*.scss', ['sass']);
+  gulp.watch('css/*.css', ['ftp-dev-css', 'ftp-css']);
+  // gulp.watch('scripts/*.js', ['scripts']);
+  // gulp.watch('js/*.js', ['ftp-dev-sripts'])
 });
+
+// Default Task
+gulp.task('default', ['watch']);
