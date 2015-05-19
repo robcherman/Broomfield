b4c212517b3a1e5115cee8b94991cee0a65e0b37
craigsatlasadvertising <craigs@atlas-advertising.com>
2015-05-19 09:51:13 -0600

    Quick link - font awesome icon correction

--- gulpfile.js
+++ gulpfile.js
@@ -10,14 +10,19 @@ var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 
 // Compile Sass
-gulp.task('sass', function() {
-  return gulp.src('build/sass/main.scss')
-    .pipe(rename({suffix: '.min'}))
-    .pipe(sass({style: 'compressed'}))
-    .pipe(gulp.dest('css'));
+gulp.task('sass', function () {
+    return gulp.src('build/sass/main.scss')
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sass({
+            style: 'compressed'
+        }))
+        .pipe(gulp.dest('css'));
 });
 
 // Dev FTP: CSS
+/*
 gulp.task('ftp-dev-css', function() {
   return gulp.src('css/*')
     .pipe(ftp({
@@ -27,8 +32,10 @@ gulp.task('ftp-dev-css', function() {
       remotePath: 'broomfield/css'
     }));
 });
+*/
 
 // Dev FTP: Sass
+/*
 gulp.task('ftp-dev-scss', function() {
   return gulp.src('scss/*')
     .pipe(ftp({
@@ -38,8 +45,10 @@ gulp.task('ftp-dev-scss', function() {
       remotePath: 'broomfield/scss'
     }));
 });
+*/
 
 // Prod FTP: CSS
+/*
 gulp.task('ftp-css', function() {
   return gulp.src('css/*')
     .pipe(ftp({
@@ -49,8 +58,11 @@ gulp.task('ftp-css', function() {
       remotePath: '/Atlas/Sites/KenticoCMS 7.0.103/broomfield/css'
     }));
 });
+*/
+
 
 // Prod FTP: Sass
+/*
 gulp.task('ftp-scss', function() {
   return gulp.src('scss/*')
     .pipe(ftp({
@@ -60,16 +72,18 @@ gulp.task('ftp-scss', function() {
       remotePath: '/Atlas/Sites/KenticoCMS 7.0.103/broomfield/scss'
     }));
 });
+*/
+
 
 // FTP Everything to Dev and Prod
-gulp.task('ftp-all', ['ftp-dev-css', 'ftp-dev-scss', 'ftp-dev-scripts', 'ftp-css', 'ftp-scss', 'ftp-scripts']);
+//gulp.task('ftp-all', ['ftp-dev-css', 'ftp-dev-scss', 'ftp-dev-scripts', 'ftp-css', 'ftp-scss', 'ftp-scripts']);
 
 // Watch Files For Changes
-gulp.task('watch', function() {
-  gulp.watch('build/sass/*.scss', ['sass']);
-  gulp.watch('css/*.css', ['ftp-dev-css', 'ftp-css']);
-  // gulp.watch('scripts/*.js', ['scripts']);
-  // gulp.watch('js/*.js', ['ftp-dev-sripts'])
+gulp.task('watch', function () {
+    gulp.watch('build/sass/*.scss', ['sass']);
+    //gulp.watch('css/*.css', ['ftp-dev-css', 'ftp-css']);
+    // gulp.watch('scripts/*.js', ['scripts']);
+    // gulp.watch('js/*.js', ['ftp-dev-sripts'])
 });
 
 // Default Task
